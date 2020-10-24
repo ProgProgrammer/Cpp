901202d66c8a067e8a048d81455d0d2db55be93f
CS <zubr.free@yandex.ru>
2020-10-24 21:52:30 +0400

    C++: created "CalculationAndProgram" program.

---
+++ StudyProjects/Basics_of_programming/Control_work_1/Chapter_6_ticket_3_Calculation_and_print/CalculationAndPrint/CalculationAndPrint.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z, q, w, q_value, w_value;
+    cout << "Enter value 'x': ";
+    cin >> x;
+    cout << "Enter value 'y': ";
+    cin >> y;
+    cout << "Enter value 'z': ";
+    cin >> z;
+
+    q_value = fmax(x*y*z, 3*z*x);
+    q_value = fmax(q_value, 2*(x+y+z));
+    if (z < 0)
+    {
+        z = -z;
+    }
+    w_value = fmin(3*x*y*z, q_value / 2);
+    printf("q = %d, w = %d", q_value, w_value);
+}
